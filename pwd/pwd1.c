437d8aaf9d2e7388f2ef07c00ac55896134606a7
root <root@lzz.fedora17>
2012-10-31 23:43:02 +0800

    update pwd1.c

--- pwd/pwd1.c
+++ pwd/pwd1.c
@@ -2,19 +2,41 @@
 #include <stdlib.h>
 #include <dirent.h>
 #include <sys/types.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+ino_t get_inode_num(const char *path)
+{
+		struct stat buf;
+		if (lstat(path, &buf))
+			return -1;
+		return buf.st_ino;
+}
 
 int main(void)
 {
 	DIR *dir;
 	struct dirent *dir_item;
+	ino_t father, child;
+	char *path;
 
-	if (!(dir = opendir("..")))
-			return -1;
+	child = get_inode_num(".");
+	father = get_inode_num("..");
 
-	if (!(dir_item = readdir(dir)))
-			return -1;
-
-	printf("%s\n", dir_item->d_name);
-
+	while (child != father) {
+			if (!(dir = opendir("..")))
+					return -1;
+			while (dir_item = readdir(dir)) {
+				if (child == dir_item->d_ino) {
+						*path = (char *)malloc(strlen(dir_item->d_name));
+						break;
+				}
+			}
+			closedir(dir);
+		chdir("../");
+		child = father;
+		father = get_inode_num("..");
+	} 
 	return 0;
 }
