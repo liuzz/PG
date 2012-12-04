8e3b2bdcd7ac10f78fd40ef1266ff83b26de8f1c
liuzhengzhen <liuzhengzhen9036@gmail.com>
2012-12-04 16:16:59 +0800

    add: ls /bin|./more

--- more/more.c
+++ more/more.c
@@ -10,7 +10,7 @@
  *       Revision:  none
  *       Compiler:  gcc
  *
- *         Author:  LiuZhengzhen (), liuzhengzhen#gmail#com
+ *         Author:  Liu Zhengzhen (), liuzhengzhen9036#gmail#com
  *   Organization:  
  *
  * =====================================================================================
@@ -29,11 +29,14 @@
 static int COLUMN;
 static int ROW;
 
-int see_more(int precent)
+static int see_more(int percent, FILE *fp)
 {
 	int c;
-	fprintf(stdout, "\033[7m--more--(%d%)\033[m", precent);
-	while ((c = getchar()) != EOF) {
+	if (percent)
+		fprintf(stdout, "\033[7m--more--(%d%)\033[m", percent);
+	else
+		fprintf(stdout, "\033[7m--more--\033[m");
+	while ((c = getc(fp)) != EOF) {
 		if (c == 'q')
 			return 0;
 		if (c == ' ')
@@ -48,11 +51,11 @@ static int term_noecho(void)
 {
 	struct termios newstat;
 
-	tcgetattr(0, &newstat);
+	tcgetattr(1, &newstat);
 	newstat.c_lflag &= ~ICANON;
 	newstat.c_lflag &= ~ECHO;
 	newstat.c_cc[VMIN] = 1;
-	tcsetattr(0, TCSANOW, &newstat);
+	tcsetattr(1, TCSANOW, &newstat);
 
 	return 0;
 }
@@ -61,9 +64,9 @@ static int termios_info(int flag)
 {
 	static struct termios oldstat;
 	if (flag == 0) {
-		tcgetattr(0, &oldstat);
+		tcgetattr(1, &oldstat);
 	} else {
-		tcsetattr(0, TCSANOW, &oldstat);
+		tcsetattr(1, TCSANOW, &oldstat);
 	}
 	return 0;
 }
@@ -82,13 +85,13 @@ static int clean_prompt(void)
 	static char *esc_sequence;
 	if (esc_sequence == NULL) {
 		char *cursor;
-		cursor = tigetstr("cup"); // This is the magic curse which you can jump to the 
+		cursor = tigetstr("cup"); // This is the magic curses which you can jump to the 
 									//cursor wherever you want, oh sorry, just limit in the terminal.
 		esc_sequence = tparm(cursor, (ROW), 0);
 	}
 
 	putp(esc_sequence);
-	fputs("                   ", stdout);
+	fputs("                 ", stdout);
 	putp(esc_sequence);
 
 	return 0;
@@ -101,32 +104,24 @@ static long long get_file_size(char *pathname)
 	return (long long)buffer.st_size;
 }
 
-static int do_more(char *filename)
+static int do_more(long long file_size, FILE *stream)
 {
-	FILE *stream;
-	char err[100];
 	char content[COLUMN];
 	int line = 0; //count the rows, when equals to ROW, pause outputting
 	long long byte_count = 0; //count for the percent
-	long long file_size; 	  
 	int replay;
-
-	file_size = get_file_size(filename);
-
-	stream = fopen(filename, "r");
-	if (!stream) {
-		sprintf(err, "Can not open file %s", filename);
-		perror(err);
-		exit(1);
-	}
+	FILE *fp_tty;
 
 	termios_info(0); //get the original termios before setting.
 	term_noecho(); 	//make the termial no echo and not need to enter "Enter"
 
+	fp_tty = fopen("/dev/tty", "r");
+	if (!fp_tty)
+		exit(1);
 	while (fgets(content, COLUMN, stream)) {
 		byte_count += strlen(content);
 		if (line == ROW) {
-			replay = see_more(((byte_count * 100)/file_size));
+			replay = see_more((file_size ? ((byte_count * 100)/file_size): 0), fp_tty);
 			clean_prompt();
 			if (replay == 0)
 				break;
@@ -142,14 +137,30 @@ static int do_more(char *filename)
 
 	termios_info(1); //recover the termios 
 	fclose(stream);
+	fclose(fp_tty);
 }
 
 int main(int argc, char *argv[]) 
 {
+	FILE *stream;
+	char err[100];
+	long long file_size;
+	FILE *fp_tty;
 	get_col_row(); //get the number of ROW and COLUMN for this special terminal,
 					// by the way run setupterm() for clean_prompt().
-	while (--argc) {
-		do_more(*++argv);
+	if (argc == 1) {
+			do_more(0, stdin);
+	} else {
+		while (--argc) {
+			stream = fopen(*++argv, "r");
+			if (!stream) {
+				sprintf(err, "Can not open file %s", *argv);
+				perror(err);
+				exit(1);
+			}
+			file_size = get_file_size(*argv);
+			do_more(file_size, stream);
+		}
 	}
 	
 	return 0;
