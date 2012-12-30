30b9df7c132b775a2febfa5f9f9ac5a40697e337
liuzz <liuzhengzhen9036@gmail.com>
2012-12-30 23:35:46 +0800

    add readproc.c and modified ls.c
    Signed-off-by: liuzz <liuzhengzhen9036@gmail.com>

--- ls/ls.c
+++ ls/ls.c
@@ -14,209 +14,281 @@
 #define TIME_END	12
 static const char *optstring = "aALh";
 static const struct option longopts[] = {
-		{"all", no_argument, NULL, 'a'},
-		{"almost-all", no_argument, NULL, 'A'},
-		{"human-readable", no_argument, NULL, 'h'},
-		{"dereference", no_argument, NULL, 'L'},
-		{"help", no_argument, NULL, '?'},
-		{0,0,0,0}	//the last element of the array has to be filled with zeros
+	{"all", no_argument, NULL, 'a'},
+	{"almost-all", no_argument, NULL, 'A'},
+	{"human-readable", no_argument, NULL, 'h'},
+	{"long-term", no_argument, NULL, 'l'},
+	{"dereference", no_argument, NULL, 'L'},
+	{"help", no_argument, NULL, '?'},
+	{0,0,0,0}	//the last element of the array has to be filled with zeros
 };
+
 enum {
-		TYPE, 
-		RUSR, WUSR, XUSR,
-		RGRP, WGRP, XGRP,
-		ROTH, WOTH, XOTH
+	TYPE, 
+	RUSR, WUSR, XUSR,
+	RGRP, WGRP, XGRP,
+	ROTH, WOTH, XOTH
 };
+
+typedef struct name_len {
+	char name[NAME_MAX+1]; 			// ctrl + ] linux/limits.h
+	unsigned short len;
+}name_len;
+
 typedef struct dirent *direntp;
+
 static int all_flag;
 static int almost_flag;
 static int link_flag;
 static int human_flag;
+static int long_flag;
 
-void get_mode(mode_t m, char *rtnmode)
+static void get_mode(mode_t m, char *rtnmode)
 {
 
-		
-		if (S_ISDIR(m))
-				rtnmode[TYPE] = 'd';
-		if (S_ISCHR(m))
-				rtnmode[TYPE] = 'c';
-		if (S_ISBLK(m))
-				rtnmode[TYPE] = 'b';
-		if (S_ISLNK(m))
-				rtnmode[TYPE] = 'l';
 
-		if (m & S_IRUSR) 
-				rtnmode[RUSR] = 'r';
-		if (m & S_IWUSR)
-				rtnmode[WUSR] = 'w';
-		if (m & S_IXUSR)
-				rtnmode[XUSR] = 'x';
+	if (S_ISDIR(m))
+		rtnmode[TYPE] = 'd';
+	if (S_ISCHR(m))
+		rtnmode[TYPE] = 'c';
+	if (S_ISBLK(m))
+		rtnmode[TYPE] = 'b';
+	if (S_ISLNK(m))
+		rtnmode[TYPE] = 'l';
 
-		if (m & S_IRGRP)
-				rtnmode[RGRP] = 'r';
-		if (m & S_IWGRP)
-				rtnmode[WGRP] = 'w';
-		if (m & S_IXGRP)
-				rtnmode[XGRP] = 'x';
+	if (m & S_IRUSR) 
+		rtnmode[RUSR] = 'r';
+	if (m & S_IWUSR)
+		rtnmode[WUSR] = 'w';
+	if (m & S_IXUSR)
+		rtnmode[XUSR] = 'x';
 
-		if (m & S_IROTH)
-				rtnmode[ROTH] = 'r';
-		if (m & S_IWOTH)
-				rtnmode[WOTH] = 'w';
-		if (m & S_IXOTH)
-				rtnmode[XOTH] = 'x';
+	if (m & S_IRGRP)
+		rtnmode[RGRP] = 'r';
+	if (m & S_IWGRP)
+		rtnmode[WGRP] = 'w';
+	if (m & S_IXGRP)
+		rtnmode[XGRP] = 'x';
+
+	if (m & S_IROTH)
+		rtnmode[ROTH] = 'r';
+	if (m & S_IWOTH)
+		rtnmode[WOTH] = 'w';
+	if (m & S_IXOTH)
+		rtnmode[XOTH] = 'x';
 }
 
-char *get_user(uid_t uid)
+static char *get_user(uid_t uid)
 {
-		struct passwd *pswd;
+	struct passwd *pswd;
 
-		pswd = getpwuid(uid);
+	pswd = getpwuid(uid);
 
-		if (pswd == NULL)
-				return "NO_USER";
+	if (pswd == NULL)
+		return "NO_USER";
 
-		return pswd->pw_name;
+	return pswd->pw_name;
 }
 
-char *get_group(gid_t gid)
+static char *get_group(gid_t gid)
 {
-		struct group *grp;
+	struct group *grp;
 
-		grp = getgrgid(gid);
+	grp = getgrgid(gid);
 
-		if (grp == NULL)
-				return "NO_GROUP";
+	if (grp == NULL)
+		return "NO_GROUP";
 
-		return grp->gr_name;
+	return grp->gr_name;
 }
 
-char *get_time(time_t mtime)
+static char *get_time(time_t mtime)
 {
-		char *rtn;
-		
-		rtn = ctime(&mtime); //Wed Jun 30 21:49:08 1993\n
-		rtn = rtn + TIME_START;
-		rtn[TIME_END] = '\0';
-		
-		return rtn;
+	char *rtn;
+
+	rtn = ctime(&mtime); //Wed Jun 30 21:49:08 1993\n
+	rtn = rtn + TIME_START;
+	rtn[TIME_END] = '\0';
+
+	return rtn;
 }
 
-char *get_name(char *name, char *modearray) 
+static char *get_name(char *name, char *modearray) 
 {
-		char *fname;
+	char *fname;
 
-		if (modearray[TYPE] == 'l') {
-				fname = (char *)malloc(PATH_MAX * 2);
-				char *buf = (char *)malloc(PATH_MAX);
+	if (modearray[TYPE] == 'l') {
+		fname = (char *)malloc(PATH_MAX * 2);
+		char *buf = (char *)malloc(PATH_MAX);
 
-				strcpy(fname, name);
-				strcat(fname, " -> ");
-				readlink(name, buf, PATH_MAX);
-				strcat(fname, buf);
-				free(buf);
+		strcpy(fname, name);
+		strcat(fname, " -> ");
+		readlink(name, buf, PATH_MAX);
+		strcat(fname, buf);
+		free(buf);
 
-				return fname;
-		}
-		else {
-				fname = (char *)malloc(PATH_MAX);
-				strcpy(fname, name);
+		return fname;
+	}
+	else {
+		fname = (char *)malloc(PATH_MAX);
+		strcpy(fname, name);
 
-				return fname;
-		}
+		return fname;
+	}
 }
 
-void print_stat(char *name)
+static void mystat(char *name, struct stat *infobuf)
 {
-		char modearray[11];
-		char *fname;
-		strcpy(modearray, "----------");
-		struct stat infobuf;
-		if (link_flag)
-				stat(name, &infobuf);
-		else 
-				lstat(name, &infobuf);
-		get_mode(infobuf.st_mode, modearray);
-		fprintf(stdout, "%s.", modearray);
-		fprintf(stdout, "%4d ", (int)infobuf.st_nlink);			
-		fprintf(stdout, "%-5s", get_user(infobuf.st_uid));
-		fprintf(stdout, "%-5s", get_group(infobuf.st_gid));
-		fprintf(stdout, "%6ld ", (long)infobuf.st_size);
-		fprintf(stdout, "%s ", get_time(infobuf.st_mtime));
-		fname = get_name(name, modearray);
-		fprintf(stdout, "%s\n", fname);
-		free(fname);
+	if (link_flag)
+		stat(name, infobuf);
+	else 
+		lstat(name, infobuf);
 }
 
-void do_list(char *dirname)
+static int is_dir(char *name)
 {
-		DIR *dirp;
-		direntp dp;
-		char *old_pwd = (char *)malloc(PATH_MAX * sizeof(char));
-		char *current_pwd = (char *)malloc(PATH_MAX * sizeof(char));
-		
-		getcwd(current_pwd, PATH_MAX);
-		strcpy(old_pwd, current_pwd);
+	struct stat infobuf;
+	mystat(name, &infobuf);
+	if (S_ISDIR(infobuf.st_mode))
+		return 1;
+	return 0;
+}
 
-		if (chdir(dirname)) {
-				printf("chdir error\n");
-				free(current_pwd);
-				free(old_pwd);
-				return;
-		}
+static void print_stat(char *name)
+{
+	char modearray[11];
+	char *fname;
+	strcpy(modearray, "----------");
+	struct stat infobuf;
+	mystat(name, &infobuf);
+	get_mode(infobuf.st_mode, modearray);
+	fprintf(stdout, "%s.", modearray);
+	fprintf(stdout, "%4d ", (int)infobuf.st_nlink);			
+	fprintf(stdout, "%-5s", get_user(infobuf.st_uid));
+	fprintf(stdout, "%-5s", get_group(infobuf.st_gid));
+	fprintf(stdout, "%6ld ", (long)infobuf.st_size);
+	fprintf(stdout, "%s ", get_time(infobuf.st_mtime));
+	fname = get_name(name, modearray);
+	fprintf(stdout, "%s\n", fname);
+	free(fname);
+}
 
-		getcwd(current_pwd, PATH_MAX);
+static int mycmp(const void *p1, const void *p2)
+{
+	name_len *n1 = *(name_len **)p1;
+	name_len *n2 = *(name_len **)p2;
+	char *s1 = n1->name;
+	char *s2 = n2->name;
+	return strcasecmp(s1, s2);
+}
 
-		dirp = opendir(current_pwd);
-			
-		while ((dp = readdir(dirp))) {
-				if ( *(dp->d_name) != '.' )
-						print_stat(dp->d_name);
-		}						
+static void print_dir_simple(name_len **table, int count)
+{
+	qsort(table, count, sizeof(name_len *), mycmp);
+	int i;
+	for (i = 0; i < count; i++)
+		printf("%s\n", table[i]->name);
+}
 
-		closedir(dirp);
+static void do_list(char *dirname)
+{
+	if (!is_dir(dirname)) {
+		print_stat(dirname);
+		return;
+	}
+	DIR *dirp;
+	direntp dp;
+	char *old_pwd = (char *)malloc(PATH_MAX * sizeof(char));
+	char *current_pwd = (char *)malloc(PATH_MAX * sizeof(char));
+	name_len **dir_item_tab = NULL;
+	int count = 0;
 
-		if (chdir(old_pwd)) {
-				printf("chdir error\n");
-		}
+	getcwd(current_pwd, PATH_MAX);
+	strcpy(old_pwd, current_pwd);
 
+	if (chdir(dirname)) {
+		printf("chdir error\n");
 		free(current_pwd);
 		free(old_pwd);
+		return;
+	}
+
+	getcwd(current_pwd, PATH_MAX);
+
+	dirp = opendir(current_pwd);
+
+	if (long_flag) {
+		while ((dp = readdir(dirp))) {
+			if ( *(dp->d_name) != '.' )
+				print_stat(dp->d_name);
+		}						
+	}
+	else {
+		while ((dp = readdir(dirp))) {
+			if ( *(dp->d_name) != '.' ) {
+				dir_item_tab = realloc(dir_item_tab, (count+1) * sizeof(name_len *));
+				dir_item_tab[count] = malloc(sizeof(name_len));
+				strcpy(dir_item_tab[count]->name, dp->d_name);   
+				dir_item_tab[count++]->len = dp->d_reclen;
+			}
+		}						
+		dir_item_tab = realloc(dir_item_tab, (count+1) * sizeof(name_len *));
+		dir_item_tab[count++] = NULL;   // There is something boring!!! I just want a NULL to terminate.
+		print_dir_simple(dir_item_tab, count-1);
+		//miss collectting for dir_item_tab
+		free(dir_item_tab);
+	}
+
+	closedir(dirp);
+
+	if (chdir(old_pwd)) {
+		printf("chdir error\n");
+	}
+
+	free(current_pwd);
+	free(old_pwd);
 }
 
-void parse_arg(int argc, char *argv[])
+static void usage(void)
 {
-		int rtn, index;
-		while ((rtn = getopt_long(argc, argv, optstring, longopts, &index)) != -1) {
-				switch (rtn) {
-						case 'a':
-								all_flag = 1;
-								break;
-						case 'A':
-								almost_flag = 1;
-								break;
-						case 'L':
-								link_flag = 1;
-								break;
-						case 'h':
-								human_flag = 1;
-								break;
-						case '?':
-								//usage();
-								break;
-				}
+	printf("ls [-aALh][directory...]\n");
+}
+static void parse_arg(int argc, char *argv[])
+{
+	int rtn, index;
+	while ((rtn = getopt_long(argc, argv, optstring, longopts, &index)) != -1) {
+		switch (rtn) {
+			case 'a':
+				all_flag = 1;
+				break;
+			case 'A':
+				almost_flag = 1;
+				break;
+			case 'l':
+				long_flag = 1;
+				break;
+			case 'L':
+				link_flag = 1;
+				break;
+			case 'h':
+				human_flag = 1;
+				break;
+			case '?':
+				usage();
+				exit(0);
 		}
+	}
 }
 
 int main(int argc, char *argv[])
 {
-		parse_arg(argc, argv);
-		int num_dir = argc - optind;
-		argc = optind;
-		while (num_dir--) {
-				do_list(*(argv + argc++));
-		}
-		return 0;
+	parse_arg(argc, argv);
+	int num_dir = argc - optind;
+	argc = optind;
+	if (num_dir == 0)
+		do_list(".");
+	while (num_dir--) {
+		do_list(*(argv + argc++));
+	}
+	return 0;
 }
-
---
+++ who/readproc.c
@@ -0,0 +1,187 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  readproc.c
+ *
+ *    Description:  
+ *    				open /proc, and read /proc/'pid'/stat
+ *
+ *        Version:  1.0
+ *        Created:  12/28/2012 01:12:38 PM
+ *       Revision:  none
+ *       Compiler:  gcc readproc.c 
+ *
+ *         Author:  Liu Zhengzhen (), liuzhengzhen9036#gmail#com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <utmp.h>
+
+#define PROC_PATH 			"/proc/"
+
+typedef struct proc_t {
+	pid_t pid;
+	char command[256];
+	char status;
+	pid_t ppid;
+	pid_t pgrp;
+	pid_t sid;
+	long tty;
+	pid_t tpgid;
+	unsigned long flags;
+	unsigned long min_flt;
+	unsigned long cmin_flt;
+	unsigned long maj_flt;
+	unsigned long cmaj_flt;
+	unsigned long long utime;
+	unsigned long long stime;
+	unsigned long long cutime;
+	unsigned long long cstime;
+	unsigned long long start_time;
+	long priority;
+	long nice;
+	long alarm;
+	int nlwp;
+}proc_t;
+
+static int is_all_num(char *ptr) 
+{
+	for (; *ptr; ptr++) 
+		if (!((*ptr <= '9') && (*ptr >= '0'))) { 		// < '9' is first tested !!! a big mistake missing =
+			return 0;	
+		}
+	return 1;
+}
+
+static int tty_name2num(char *name) 
+{
+	char ttyname[32];
+	struct stat buffer;
+	snprintf(ttyname, 32, "/dev/%s", name);	
+	if (stat(ttyname, &buffer) >=0) return buffer.st_rdev;
+	snprintf(ttyname, 32, "/dev/pts/%s", name+3);
+	if (stat(ttyname, &buffer) >=0) return buffer.st_rdev;
+}
+
+int main(void)
+{
+	DIR *dir;
+	struct dirent *diritem;
+	FILE *stream;
+	struct utmp *user;
+	int count = 0;
+	int c;
+	int n;
+	proc_t **proc_stat_table = NULL;
+	if (!(dir = opendir(PROC_PATH))) {
+		perror("can not open /proc");
+		exit(1);
+	}
+	while (diritem = readdir(dir)) {
+		char *dirname = malloc(PATH_MAX);
+		struct stat buffer;
+		proc_t *proc_stat = malloc(sizeof (proc_t));
+		strcpy(dirname, PROC_PATH);
+		strcat(dirname, diritem->d_name);
+		if (stat(dirname, &buffer)) {
+			perror("can not stat");
+			exit(2);
+		}
+		if (!S_ISDIR(buffer.st_mode)) {	// wether it is a dir : stat.h ctrl + ]
+			goto final;
+		} 
+		if (!(is_all_num(diritem->d_name))) {
+			goto final;
+		}
+		strcat(dirname, "/stat");
+		if (!(stream = fopen(dirname, "r"))) {
+			perror("can not fopen");
+			goto final;	
+		}
+		fscanf(stream, "%ld %s %s " 
+				"%ld %ld %ld" 
+				"%ld %ld " 
+				"%lu %lu %lu %lu "
+				"%lu %Lu %Lu %Lu "
+				"%Lu %ld "
+				"%ld %d %lu "
+				"%Lu", 
+				&(proc_stat->pid), &(proc_stat->command), &(proc_stat->status), 
+				&(proc_stat->ppid), &(proc_stat->pgrp), &(proc_stat->sid),
+				&(proc_stat->tty), &(proc_stat->tpgid),
+				&(proc_stat->flags), &(proc_stat->min_flt), &(proc_stat->cmin_flt), &(proc_stat->maj_flt), 
+				&(proc_stat->cmaj_flt), &(proc_stat->utime), &(proc_stat->stime), &(proc_stat->cutime), 
+				&(proc_stat->cstime), &(proc_stat->priority), 
+				&(proc_stat->nice), &(proc_stat->nlwp), &(proc_stat->alarm), 
+				&(proc_stat->start_time));
+
+		fclose(stream);
+		if (proc_stat->tty <= 0) {
+			goto final;
+		}
+		char *temp = strrchr(dirname, '/');
+		*temp = 0;
+		strcat(dirname, "/cmdline");
+		int fd = open(dirname, O_RDONLY);
+		if (fd < 0) 	goto final;
+		char buf[64];
+		n = read(fd, buf, 64);
+		for (c = 0; c < n - 1; c++) {
+			if (buf[c] == 0) buf[c] = ' ';
+		}
+		buf[n-1] = 0;
+		strcpy(proc_stat->command, buf);	
+		proc_stat_table = realloc(proc_stat_table, (count+1) * sizeof (proc_t *));
+		if (!proc_stat_table) {
+			perror("cannot realloc\n");
+			exit(1);
+		}
+		proc_stat_table[count] = malloc(sizeof(proc_t));
+		memcpy(proc_stat_table[count], proc_stat, sizeof(proc_t));
+		count++;
+final:
+		free(dirname);
+		free(proc_stat);
+		continue;
+		//strtoul(dirent->d_name, NULL, 10);		
+	}
+	utmpname(UTMP_FILE); 			//utmp.h ctrl + ]
+	setutent();
+	int inter;
+	while (user=getutent()) {
+		proc_t *best = NULL;
+		if (user->ut_type != USER_PROCESS) continue;
+		long tty_num = tty_name2num(user->ut_line);
+		//printf("%s %ld\n", user->ut_user, tty_num);
+		for (inter = 0; inter < count; inter++) {
+			if (tty_num == proc_stat_table[inter]->tty)
+				if (proc_stat_table[inter]->pgrp == proc_stat_table[inter]->tpgid) {
+					if (!best) {
+						best = malloc(sizeof(proc_t));
+						memcpy(best, proc_stat_table[inter], sizeof(proc_t));	
+					} else if (proc_stat_table[inter]->start_time > best->start_time){
+						memcpy(best, proc_stat_table[inter], sizeof(proc_t));	
+					}
+				}
+		}
+		printf("%-*s %-*s %-*s\n", 8, user->ut_user, 8,
+									user->ut_line, 10,
+									best->command);
+		free(best);
+	}
+	endutent();
+	for (; count;)	
+		free(proc_stat_table[--count]);
+	free(proc_stat_table);
+	return 0;
+}
